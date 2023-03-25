fddbc98d5ccc834ccb586694f485c19d8731ac5f
tuyen <tuyen.le@sotatek.com>
2023-03-25 18:01:22 +0700

    fix: bug tailwin

--- src/components/common/Paginate.tsx
+++ src/components/common/Paginate.tsx
@@ -27,13 +27,13 @@ const PaginatedItems = memo(function PaginatedItems({
                 pageCount={pageCount}
                 previousLabel="Previous"
                 renderOnZeroPageCount={undefined}
-                className="text-azure inline-flex border rounded-md"
-                pageClassName="w-[30px] md:w-[40px] center text-center border-r hover:shadow-xl hover:bg-text-azure hover:bg-azure hover:bg-opacity-40"
+                className="text-[#007BFF] inline-flex border rounded-md"
+                pageClassName="w-[30px] md:w-[40px] center text-center border-r hover:shadow-xl hover:bg-text-[#007BFF] hover:bg-[#007BFF] hover:bg-opacity-40"
                 previousClassName="text-center md:py-2 px-3 border-r"
                 nextClassName="text-center md:py-2 px-3 border-l"
-                activeClassName="text-white bg-azure hover:!bg-opacity-100"
+                activeClassName="text-white bg-[#007BFF] hover:!bg-opacity-100"
                 pageLinkClassName="w-full h-full flex  justify-center items-center"
-                breakClassName="w-[30px] md:w-[40px]  center text-center border-r hover:shadow-xl hover:bg-text-azure hover:bg-azure hover:bg-opacity-40"
+                breakClassName="w-[30px] md:w-[40px]  center text-center border-r hover:shadow-xl hover:bg-text-[#007BFF] hover:bg-[#007BFF] hover:bg-opacity-40"
                 breakLinkClassName="w-full h-full  flex justify-center items-center"
             />
         </div>
